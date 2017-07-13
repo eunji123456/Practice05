4ecfb6469caed19badf0e3a31b7a6b44a71fae50
BIT <BIT@DESKTOP-DOGVD1T>
2017-07-13 12:33:00 +0900

    3

--- src/com/javaex/problem03/Goods.java
+++ src/com/javaex/problem03/Goods.java
@@ -6,6 +6,42 @@ public class Goods {
     private int price;
     private int count;
     
-    
-    
+    public Goods() {}
+    public Goods(String name ,int price ,int count) {
+    	this.name=name;
+    	this.price=price;
+    	this.count=count;
+    	
+    }
+    public void setname (String name) {
+    	this.name = name;
+    	
+    }
+    public void setprice (int price) {
+    	this.price = price;
+    	
+    }
+    public void price (int count) {
+    	this.count = count;
+    	
+    }
+    public int count () {
+    	return count;
+    	
+    }
+    public String getname () {
+    	return name;
+    	
+    } public int getprice () {
+    	return price;
+    	
+    }
+    public int sum() {
+    	int sum=+ price;
+    	return sum;
+    }
+    public void showinfo() {
+    	 String a ="이름:"+name+" 가격:"+price+"   갯수:"+count;
+    	 System.out.println(a);
+    }
 }
--- src/com/javaex/problem03/GoodsApp.java
+++ src/com/javaex/problem03/GoodsApp.java
@@ -7,7 +7,26 @@ import java.util.Scanner;
 public class GoodsApp {
 
     public static void main(String[] args) {
-
+    	System.out.println("상품 3개를 입력해주세요.");
+    	Scanner sc = new Scanner (System.in);
+    
+    	List<Goods> glist = new ArrayList<Goods>();
+    	int count=0;
+    	
+    	for(int i =0;i<3;i++) {
+    	String data = sc.nextLine();
+    	String []SpData = data.split(" ");
+    	
+    	Goods goods = new Goods(SpData[0],Integer.parseInt(SpData[1]),Integer.parseInt(SpData[2]));
+    
+    	glist.add(goods);
+    	
+    	}
+    	for(int i =0;i<glist.size();i++) {
+    		glist.get(i).showinfo();
+    		count += glist.get(i).count();
+    	}
+    	System.out.println("갯수는 : " +count+"개입니다.");
        
     }
 
